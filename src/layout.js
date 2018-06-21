86923798cc2abd5d7c8329170e485dcb1ea17065
Jordan Liebman <jordanliebman329@gmail.com>
2018-06-20 22:29:34 -0500

    initial

---
+++ src/layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className=''>
+        
+      </div>
+    );
+  }
+}
+
+export default Layout;
